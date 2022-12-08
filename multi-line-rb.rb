6b7922e0991c243b4706b3df96dbf98bce9abdf7
Ishwar Kanse <ikanse@redhat.com>
2022-12-08 12:01:22 +0530

    raise multiline error

--- multi-line-rb.rb
+++ multi-line-rb.rb
@@ -2,10 +2,11 @@ x = 1
 until x == 2
   puts("Hello, World !")
   begin
-    File.open("/test/hosts") {|f| f.close; f.read }
-  rescue => e
+    raise RuntimeError.new("multi-line-rb.rb:4:in `initialize': No such file or directory @ rb_sysopen - /unexistant_file (Errno::ENOENT)
+	from multi-line-rb.rb:4:in `open'
+	from multi-line-rb.rb:4:in `<main>'")
+  rescue RuntimeError => e
     puts e.message
-    puts e.backtrace
   end
   sleep 1
 end
